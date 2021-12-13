fafb24e1d8f2fadbf8d5e0fdef288370b2bd9468
SimonG <simon.grunwald@outlook.com>
2021-12-14 09:40:27 +1000

    Added html code from old server

--- build/html/index.php
+++ build/html/index.php
@@ -1,17 +1,22 @@
 <html>
   <head>
+    <meta name="viewport" content="width=device-width" />
     <title>Backup Server Control Panel</title>
   </head>
-  
   <body>
     <form method="get" action="shutdown.php">
-      <input type="submit" value="SHUTDOWN PI" name="shutdown">
-      <input type="submit" value="RESTART PI" name="restart">
+        <input type="submit" value="SHUTDOWN PI" name="shutdown">
+        <input type="submit" value="RESTART PI" name="restart">
     </form>
-    
-    Your current local IP address is
+
+    Your current local IP address is:
     <?php
-      echo $_SERVER['REMOTE_ADDR'];
+        echo $_SERVER['REMOTE_ADDR'];
     ?>
+    <p>
+    <form action="bbl.php">
+        <input type="submit" value="Big Bash League 11 Stats">
+    </form>
   </body>
 </html>
+
