4504a011cfd8436a303fe3f83cb85551914fe4a2
Simon <61439947+Simon536@users.noreply.github.com>
2021-12-14 09:23:49 +1000

    Update shutdown.php

--- build/html/shutdown.php
+++ build/html/shutdown.php
@@ -1,5 +1,21 @@
 <html>
   <head>
+    <meta name="viewport" content="width=device-width" />
     <title>Shutting Down...</title>
   </head>
+  <body>
+    <?php
+        if(isset($_GET['shutdown'])){
+            echo "Shutting down in one minute...";
+            echo "Please wait at least 2 minutes before turning power off!";
+            system('sudo /sbin/shutdown -h -P +1');
+        }
+        else if(isset($_GET['restart'])){
+            echo "Restarting in one minute...";
+            echo "Please close this tab and wait approximately 3 minutes for re$
+            system('sudo /sbin/shutdown -r +1');
+        }
+    ?>
+  </body>
 </html>
+
