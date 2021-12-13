cc37d755f3102b74d284bad0ec48291034fab99c
Simon <61439947+Simon536@users.noreply.github.com>
2021-12-14 09:16:27 +1000

    Update index.php

--- build/html/index.php
+++ build/html/index.php
@@ -4,6 +4,11 @@
   </head>
   
   <body>
+    <form method="get" action="shutdown.php">
+      <input type="submit" value="SHUTDOWN PI" name="shutdown">
+      <input type="submit" value="RESTART PI" name="restart">
+    </form>
+    
     Your current local IP address is
     <?php
       echo $_SERVER['REMOTE_ADDR'];
