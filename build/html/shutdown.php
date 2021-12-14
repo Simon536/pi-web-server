887a82c0cf3ef4a42eff8fb7ad44134640c7c357
Simon <61439947+Simon536@users.noreply.github.com>
2021-12-14 12:45:18 +1000

    Update shutdown.php

--- build/html/shutdown.php
+++ build/html/shutdown.php
@@ -8,11 +8,13 @@
         if(isset($_GET['shutdown'])){
             echo "Shutting down...";
             echo "Please wait at least 1 minute before turning power off!";
+            echo "<form method="get" action="shutdown.php"><input type="submit" value="CANCEL" name="cancel"></form>";
             system('sudo /sbin/shutdown -P +1');
         }
         else if(isset($_GET['restart'])){
             echo "Restarting...";
             echo "Please close this tab and wait for restart to complete!";
+            echo "<form method="get" action="shutdown.php"><input type="submit" value="CANCEL" name="cancel"></form>";
             system('sudo /sbin/shutdown -r +1');
         }
         else if(isset($_GET['cancel'])){
